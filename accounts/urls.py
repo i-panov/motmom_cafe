2d448411089ba0e13cfad6b8340a4c825e450243
Панов Илья <panov210@yandex.ru>
2017-09-11 20:25:25 +0300

    Набросал примерные вьюхи и шаблоны для заказа, профиля и статистики.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,18 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
+from . import views
+
+
+app_name = 'accounts'
+
 
 urlpatterns = [
-    url(r'^login/', auth_views.LoginView.as_view(template_name='accounts/login.html')),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    url(r'profile/$', views.profile, name='profile'),
+    url(r'^statistics/$', views.statistics, name='statistics'),
+    url(r'^order/$', views.order, name='order'),
+    # url(r'^order/(?P<token>.+)/$', views.order, name='order'),
+    url(r'^set_order_status/$', views.set_order_status, name='set_order_status'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from urllib.parse import unquote_plus
 
-# Create your views here.
+from .models import Order
+
+
+def profile(request):
+    orders = Order.objects.filter(user=request.user)
+
+    return render(request, 'accounts/profile.html', {
+        'orders': orders,
+        'sum': sum(o.sum for o in orders),
+        'user': request.user
+    })
+
+
+def statistics(request):
+    orders = list(Order.objects.all())
+    # years = [o. for o in orders]
+
+    result = {
+        'years': {
+            'year': 2017,
+            'employees': [
+                {
+                    'name': 'Tom',
+                    'months': [100, 200]
+                }
+            ]
+        }
+    }
+
+    return render(request, 'accounts/statistics.html')
+
+
+def order(request):
+    token = request.GET.get('token')
+    current = Order.from_token(token)
+
+    if not request.user.is_superuser and request.user != current.user:
+        redirect('/')
+
+    return render(request, 'accounts/order.html')
+
+
+def set_order_status(request):
+    order = Order.objects.get(pk=request.POST['id'])
+    order.completed = request.POST['status']
+    order.save()
