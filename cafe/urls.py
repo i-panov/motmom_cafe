f2dd7b542bb38a3e7e2f6eeee351c9917c0d4685
Панов Илья <panov210@yandex.ru>
2017-09-12 11:06:02 +0300

    Исправил ссылки

--- cafe/urls.py
+++ cafe/urls.py
@@ -8,6 +8,6 @@ app_name = 'cafe'
 
 urlpatterns = [
     url(r'^$', views.most_popular, name='most_popular'),
-    url(r'^(?P<pk>[0-9]+)/$', views.category, name='category'),
+    url(r'^category/(?P<pk>[0-9]+)/$', views.category, name='category'),
     url(r'^cart/$', views.cart, name='cart'),
 ]
